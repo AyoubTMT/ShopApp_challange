fe1ae11e27a9c86bcd4bd681f8ed4392fe9964d2
Ayoub <mohamed.tajmout@gmail.com>
2019-01-12 14:59:02 +0100

    Dislike a shop hide it within Nearby shops list during the next 2 hours

--- resources/views/pages/shops.blade.php
+++ resources/views/pages/shops.blade.php
@@ -14,7 +14,7 @@
 
         @if (count($shops) > 0)
             @foreach ($shops as $shop)
-                <div class="col">
+                <div class="col" id="{{$shop->id}}">
                     <div class="card  text-center" id="shop" style="width: 18rem;">
                         <div class="card-header">{{$shop->shopname}} - {{$shop->location}}m</div>
                         <div class="card-body">
@@ -28,16 +28,32 @@
                                     {!!Form::close()!!}
                                 </div>
                                 <div class="d-inline-block">
-                                    <!-- Dislike a shop -->
+                                    <button  type="button" class="btn btn-danger" onclick="myFunction({{$shop->id}})">Dislike</button>
+                                    {{-- <!-- Dislike a shop -->
                                     {!!Form::open(['action'=>['PreferedShopsController@store',$shop->id],'method'=>'POST','class'=>'pull-right'])!!}
                                         {{Form::submit('Dislike',['class'=>'btn btn-danger'])}}
-                                    {!!Form::close()!!}
+                                    {!!Form::close()!!} --}}
                                 </div>
                             @endif
                         </div>
                     </div>
                 </div>
             @endforeach
+            {{-- this script is executed when we click on Dislike button --}}
+            <script>
+                function myFunction(id) {
+                    var currentTime = ''
+                    var hours = new Date(new Date().getTime()+2*3600*1000);
+                    var newElement = document.getElementById(id);
+                    document.cookie='Dislike_Cookie=true;expires='+hours+';path=/';
+                    //document.getElementById(nme).style.display='none';
+                             
+                    if(hours > currentTime) {
+                    newElement.style.display = "none";
+                    }
+                    newElement.style.visibility = '';
+                }
+            </script>
         @else
             <p>No shops found !!!</p>
         @endif
